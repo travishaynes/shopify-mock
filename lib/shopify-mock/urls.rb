6bb655e24c1f08510930b9c993ebb14fc0f7091e
Travis Haynes <travis.j.haynes@gmail.com>
2011-09-07 12:50:46 -0700

    mocks any shopify shop domain instead targetting test.myshopify.com specifically

--- lib/shopify-mock/urls.rb
+++ lib/shopify-mock/urls.rb
@@ -1,7 +1,1 @@
-SHOPIFY_MOCK_SHOP = {
-  :domain   => "test.myshopify.com",
-  :api_key  => "apikey",
-  :secret   => "secret"
-}
-
-SHOPIFY_MOCK_SHOP_BASE_URL = "https://.*:.*@#{SHOPIFY_MOCK_SHOP[:domain]}/admin/"
+SHOPIFY_MOCK_SHOP_BASE_URL = "https://.*:.*@.*/admin/"
--- spec/support/http_support.rb
+++ spec/support/http_support.rb
@@ -1,9 +1,9 @@
-def get(path = [], options = {}, user = SHOPIFY_MOCK_SHOP[:api_key], pass = SHOPIFY_MOCK_SHOP[:secret])
+def get(path = [], options = {}, user = "api_key", pass = "token")
   path = parse_path(path)
   options = parse_options(options)
   path = "#{path}?#{options}" unless options.nil?
   
-  http = Net::HTTP.new(SHOPIFY_MOCK_SHOP[:domain], 443)
+  http = Net::HTTP.new("test.myshopify.com", 443)
   http.use_ssl = true
   req = Net::HTTP::Get.new path
   req.basic_auth user, pass
