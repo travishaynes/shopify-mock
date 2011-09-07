54d893a59ce6cd002407b7adaccea0073698f940
Travis Haynes <travis.j.haynes@gmail.com>
2011-09-06 18:32:59 -0700

    fixtures cache, and can be overridden

--- lib/mockify/fixtures.rb
+++ lib/mockify/fixtures.rb
@@ -1,9 +1,21 @@
 module Mockify
   class Fixtures
-    def self.read(name, ext = :json)
-      fixture = File.expand_path("../fixtures/#{name.to_s}.#{ext.to_s}", __FILE__)
-      raise "invalid fixture name" unless File.exists? fixture
-      File.read(fixture)
+    @cache = {}
+    
+    class << self
+      
+      def read(name, ext = :json)
+        fixture_name = "#{name.to_s}.#{ext.to_s}"
+        fixture = File.expand_path("../fixtures/#{fixture_name}", __FILE__)
+        raise "invalid fixture name" unless File.exists? fixture
+        @cache[fixture_name] = File.read(fixture) unless @cache.include? fixture_name
+        @cache[fixture_name]
+      end
+    
+      def use(name, content)
+        @cache["#{name}.json"] = content
+      end
+      
     end
   end
 end
--- spec/mockify/fixtures_spec.rb
+++ spec/mockify/fixtures_spec.rb
@@ -4,14 +4,25 @@ describe Mockify::Fixtures do
   
   context "when given a valid fixture name" do
     it "should return the contents of a fixture" do
-      @xml = read_fixture :test
-      Mockify::Fixtures.read(:test).should eq @xml
+      @json = read_fixture :test
+      Mockify::Fixtures.read(:test).should eq @json
     end
   end
   
   context "when given an invalid fixture name" do
     it "should raise an error" do
-      expect { Fixtures.read(:brown_chicken_brown_cow) }.should raise_error
+      expect { Mockify::Fixtures.read(:brown_chicken_brown_cow) }.should raise_error
+    end
+  end
+  
+  context "custom fixtures" do
+    before { @json = '{ "count": 10 }' }
+    describe "#use" do
+      it "should override default fixture" do
+        Mockify::Fixtures.read(:orders).should eq read_fixture :orders
+        Mockify::Fixtures.use :count, @json
+        Mockify::Fixtures.read(:count).should eq @json
+      end
     end
   end
   
