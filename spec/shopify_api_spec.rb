95834fa9169cb3d5f9ebf02a7bdee83cf61502ec
Mr Rogers <jon@bunnymatic.com>
2011-09-13 19:19:15 -0700

    * added mock responses to handle get by id requests ( REST requests like <object>/<id>.json )  * added corresponding tests

--- spec/shopify_api_spec.rb
+++ spec/shopify_api_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 shared_examples "a collection" do
-  let(:collection) { described_class.all }
+  let(:collection) { 
+    described_class.all 
+  }
   
   describe "#all" do
     it "should be an Array of #{described_class}" do
@@ -21,12 +23,12 @@ describe "ShopifyAPI objects" do
   end
   
   # test collections
-  [ :asset, :blog, :comment, :country, :custom_collection, :customer_group,
+  [ :blog, :comment, :country, :custom_collection, :customer_group,
     :customer, :order, :page, :product_search_engine, :product, :redirect,
-    :script_tag, :smart_collection, :webhook
+    :script_tag, :smart_collection, :theme, :webhook
   ].each do |o|
     
-    @class = "ShopifyAPI::" << o.to_s.split("_").collect(&:capitalize).join
+    @class = "ShopifyAPI::" << o.to_s.classify
     @class = @class.constantize
     
     describe @class do
@@ -34,16 +36,35 @@ describe "ShopifyAPI objects" do
     end
     
   end
-  
+
+  # still to test
+  # :articles, :events, :fulfillments,:variants, :transactions :provinces, :images, :metafields,
+  # test find on classes which have ids
+  [:blogs, :comments, :countries, :custom_collections, :customer_groups,
+   :customers,  :orders, :pages, :products,  :redirects, :script_tags, 
+   :smart_collections, :themes, :webhooks].each do |o|
+
+    it "find #{o}/:id.json should return a ShopifyAPI::#{o.to_s.singularize.classify} item with the right id" do
+    
+      clz = "ShopifyAPI::" << o.to_s.singularize.classify
+      clz = clz.constantize
+    
+      first_item = JSON.parse(ShopifyAPI::Mock::Fixture.find(o, :json).data)[o.to_s].first
+      found = clz.find(first_item['id'].to_i)
+      found.should be_a clz
+      found.id.should == first_item['id']
+    end
+  end
+
   describe ShopifyAPI::Shop do
     before { @shop = ShopifyAPI::Shop.current }
     specify { @shop.should be_a ShopifyAPI::Shop }
   end
   
   # TODO: test these objects:
-  # :application_charge, :article, :collect, :event, :fulfillment,
+  # :asset, :application_charge, :article, :collect, :event, :fulfillment,
   # :image, :metafield, :province, :recurring_application_charge, 
   # :transaction, :variant
-  
+
   
 end
